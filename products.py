487f24859c7275fd56b0483de58033a224feaca0
Jason Liu <sml42j@outlook.com>
2021-02-21 23:08:24 +0800

    Add write file function

--- products.py
+++ products.py
@@ -5,7 +5,12 @@ while True:
         break
     price = input('Please key in your product price :')
     products.append([name, price])
-print(products)
+#print(products)
+
 
 for p in products:
     print(p)
+
+with open('products.csv', 'w') as f:
+    for p in products:
+        f.write(p[0] + ', ' + p[1] + '\n')
